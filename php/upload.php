3841b94014fd0eab2d439c8559744d018bea3d11
Samuel <124095857+SamuelMorais45@users.noreply.github.com>
2023-08-16 15:57:43 -0300

    tentativa de conserta o erro

--- php/upload.php
+++ php/upload.php
@@ -1,63 +1,57 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $targetDir = "arquivos/justificativa";
+    $targetDir = "arquivos/justificativa/";
     $targetFile = $targetDir . basename($_FILES["file"]["name"]);
     $uploadOk = 1;
     $imageFileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "marmita";
-    
-    if (isset($_POST["submit"])) {
-        $check = getimagesize($_FILES["file"]["tmp_name"]);
-        if ($check !== false) {
-            echo "O arquivo é uma imagem - " . $check["mime"] . ".";
-            $uploadOk = 1;
-        } else {
-            echo "O arquivo não é uma imagem.";
-            $uploadOk = 0;
-        }
+    $allowedFileTypes = array("pdf");
+    $maxFileSize = 5 * 1024 * 1024; // 5 MB
+
+    // Verificar se o upload ocorreu sem erros
+    if ($_FILES["file"]["error"] !== UPLOAD_ERR_OK) {
+        echo "Erro ao fazer o upload do arquivo.";
+        exit;
     }
-    if (file_exists($targetFile)) {
-        echo "Desculpe, o arquivo já existe.";
-        $uploadOk = 0;
-    }
-    $maxFileSize = 5 * 1024 * 1024;
-    if ($_FILES["file"]["size"] > $maxFileSize) {
-        echo "Desculpe, o tamanho do arquivo é muito grande.";
+
+    // Verificar o tipo de arquivo e o tamanho
+    if (!in_array($imageFileType, $allowedFileTypes) || $_FILES["file"]["size"] > $maxFileSize) {
+        echo "Desculpe, o arquivo enviado não é um PDF válido ou é muito grande.";
         $uploadOk = 0;
     }
 
-    $allowedFileTypes = array("pdf");
-    if (!in_array($imageFileType, $allowedFileTypes)) {
-        echo "Desculpe, apenas arquivos PDF são permitidos.";
-        $uploadOk = 0;
-    }
     if ($uploadOk == 0) {
         echo "Desculpe, seu arquivo não foi enviado.";
     } else {
         if (move_uploaded_file($_FILES["file"]["tmp_name"], $targetFile)) {
             echo "O arquivo " . basename($_FILES["file"]["name"]) . " foi enviado.";
+
+            // Conexão com o banco de dados
+            $servername = "localhost";
+            $username = "root";
+            $password = "";
+            $dbname = "marmita";
+
+            $conn = new mysqli($servername, $username, $password, $dbname);
+
+            if ($conn->connect_error) {
+                die("Falha na conexão com o banco de dados: " . $conn->connect_error);
+            }
+
+            // Usar consulta preparada para evitar injeção de SQL
+            $stmt = $conn->prepare("INSERT INTO arquivos (caminho) VALUES (?)");
+            $stmt->bind_param("s", $targetFile);
+
+            if ($stmt->execute()) {
+                echo "O caminho do arquivo foi salvo no banco de dados.";
+            } else {
+                echo "Erro ao salvar o caminho do arquivo no banco de dados.";
+            }
+
+            $stmt->close();
+            $conn->close();
         } else {
             echo "Desculpe, houve um erro ao enviar o seu arquivo.";
         }
     }
-        $conn = new mysqli($servername, $username, $password, $dbname);
-
-        if ($conn->connect_error) {
-            die("Falha na conexão com o banco de dados: " . $conn->connect_error);
-        }
-        $sql = "INSERT INTO arquivos (caminho) VALUES ('$targetFile')";
-
-        if ($conn->query($sql) === true) {
-            echo "O caminho do arquivo foi salvo no banco de dados.";
-        } else {
-            echo "Erro ao salvar o caminho do arquivo no banco de dados: " . $conn->error;
-        }
-
-        $conn->close();
-    } else {
-        echo "Desculpe, houve um erro ao enviar o seu arquivo.";
-    }
+}
 ?>
