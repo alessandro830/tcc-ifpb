f3ca2ab0452d7a6efb210321532bce0a10d30703
Samuel <124095857+SamuelMorais45@users.noreply.github.com>
2023-08-22 08:16:16 -0300

    atualizando upload.php

--- falta_justificativa.php
+++ falta_justificativa.php
@@ -1,42 +1,58 @@
 <?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $targetDir = "arquivos/justificativa/";
+    $targetFile = $targetDir . basename($_FILES["arquivo"]["name"]);
+    $uploadOk = 1;
+    $imageFileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
+    $allowedFileTypes = array("pdf");
+    $maxFileSize = 5 * 1024 * 1024; // 5 MB
+    $caminho_A = $targetFile;
 
-include("php/protect.php");
+    // Verificar se o upload ocorreu sem erros
+    if ($_FILES["arquivo"]["error"] !== UPLOAD_ERR_OK) {
+        echo "Erro ao fazer o upload do arquivo.";
+        exit;
+    }
 
+    // Verificar o tipo de arquivo e o tamanho
+    if (!in_array($imageFileType, $allowedFileTypes) || $_FILES["file"]["size"] > $maxFileSize) {
+        echo "Desculpe, o arquivo enviado não é um PDF válido ou é muito grande.";
+        $uploadOk = 0;
+    }
+
+    if ($uploadOk == 0) {
+        echo "Desculpe, seu arquivo não foi enviado.";
+    } else {
+        if (move_uploaded_file($_FILES["file"]["tmp_name"], $targetFile)) {
+            echo "O arquivo " . basename($_FILES["file"]["name"]) . " foi enviado.";
+
+            // Conexão com o banco de dados
+            $servername = "localhost";
+            $username = "root";
+            $password = "usbw";
+            $dbname = "marmita";
+
+            $conn = new mysqli($servername, $username, $password, $dbname);
+
+            if ($conn->connect_error) {
+                die("Falha na conexão com o banco de dados: " . $conn->connect_error);
+            }
+
+            // Usar consulta preparada para evitar injeção de SQL
+            $stmt = $conn->prepare("INSERT INTO alunos (caminho) VALUES ($caminho_A)");
+            $stmt->bind_param("s", $targetFile);
+
+            if ($stmt->execute()) {
+                echo "O caminho do arquivo foi salvo no banco de dados.";
+            } else {
+                echo "Erro ao salvar o caminho do arquivo no banco de dados.";
+            }
+
+            $stmt->close();
+            $conn->close();
+        } else {
+            echo "Desculpe, houve um erro ao enviar o seu arquivo.";
+        }
+    }
+}
 ?>
-
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/falta_just.css">
-    <link rel="stylesheet" href="css/navbar.css">
-    <link rel="apple-touch-icon" sizes="180x180" href="images/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="images/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="images/favicon-16x16.png">
-    <title>Falta/justificativa</title>
-</head>
-<body>
-    <header>
-        <img src="images/ifpb-horizontal200.png" alt="logo horizontal do ifpb">
-        <div class="nav_buttons">
-            <a href="aluno.php"><button>Voltar</button></a>
-        </div>
-    </header>
-    <div class="box">
-        <h1>Justificativa</h1>
-        <form action="php/upload.php" method="post" enctype="multipart/form-data">
-            <label for="justi">Justificativa de falta</label>
-            <input type="text" name="justi" id="justi" required>
-            <label for="arquivo" class="label_arq" id="index-label">Indexar arquivo (PDF)</label>
-            <input type="file" name="arquivo" id="arquivo" onchange="updateFileName(this)" required>
-            <div id="file-name"></div>
-            <input type="submit" value="Enviar">
-        </form>
-    </div>
-
-    <script src="js/script.js"></script>
-
-</body>
-</html>
