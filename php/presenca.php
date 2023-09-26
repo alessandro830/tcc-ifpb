f521ecf5c54fc086e256efcdc32fe0dc62d0c733
Samuel <124095857+SamuelMorais45@users.noreply.github.com>
2023-09-26 20:32:53 -0300

    Deixando certo para inserção

--- php/presenca.php
+++ php/presenca.php
@@ -3,19 +3,20 @@ $servername = "localhost";
 $username = "root";
 $password = "usbw";
 $dbname = "marmita";
-$presenca = $_POST['presenca'];
+$presenca = $_POST['veio'];
 
     if($presenca == 'sim'){
         $quant = 0;
         $connect = mysqli_connect("localhost","root","usbw","marmita");
         $insert = "insert into faltas(falta_aluno) values('$quant')";
         $result = mysqli_query($connect,$insert);
-        var_dump($result);
+        header('location:../funcionario.php');
     }else{
         $quant = 1;
         $connect = mysqli_connect("localhost","root","usbw","marmita");
         $insert = "insert into faltas(falta_aluno) values('$quant')";
         $result = mysqli_query($connect,$insert);
-        var_dump($result);
-}
+        
+    }
+      
 ?>
