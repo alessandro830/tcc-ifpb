72ac2472c4d980e2ed94118aad822ca5f4529461
Magno <107131648+M4ggno@users.noreply.github.com>
2023-09-27 09:41:57 -0300

    Update popup.js

--- js/popup.js
+++ js/popup.js
@@ -1,3 +1,6 @@
+// inicio do JS para o pop-up de marcar o almoço
+
+// Pegando as tags HTML por ID e Class
 var confMsg = document.querySelector('.conf-msg')
 var saveDays = document.querySelector('.button_save')
 var btnYes = document.querySelector('#sim')
@@ -19,6 +22,8 @@ window.onload = function () {
         }
     })
 
+    // Mensagens para caso tenha algum checkbox marcado ou não
+
     if (localStorage.saveBtn == 'true' && ind > 0){
         confMsg.querySelector('p').innerText = "Deseja marcar os seguintes dias: " + selectedDays.join(', ') + "?"
         confMsg.showModal()
@@ -29,6 +34,8 @@ window.onload = function () {
         localStorage.saveBtn = 'false'
     }
 };
+
+// Mensagens quando é selecionado sim ou não
 
 btnYes.onclick = function () {
     confMsg.querySelector('p').innerText = "Confirmado."
@@ -50,3 +57,22 @@ confMsg.querySelector('#ok').onclick = function () {
 confMsg.close()
 localStorage.saveBtn = 'false'
 }
+
+// Fim do JS para o pop-up de marcar o almoço
+
+// Inicio do JS para o pop-up do comentário
+
+document.addEventListener('DOMContentLoaded', function () {
+    var confMsg002 = document.querySelector('#conf-msg002');
+    var saveDays002 = document.querySelector('.button_comment');
+
+    saveDays002.onclick = function() {
+        localStorage.saveBtn002 = 'true';
+    };
+
+    if (localStorage.saveBtn002 == 'true') {
+        confMsg002.querySelector('p').innerText = "Comentário enviado com sucesso!";
+        confMsg002.showModal();
+        localStorage.saveBtn002 = 'false';
+    }
+});
