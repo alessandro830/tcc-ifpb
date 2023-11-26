1ace814e19dfbecde68f58407be6958bc6861cb5
Alessandro Viana <116918756+alessandro830@users.noreply.github.com>
2023-11-26 13:20:25 -0300

    erro no popup da página do funcionário corrigido

--- funcionario.php
+++ funcionario.php
@@ -14,6 +14,7 @@ include('php/func_dia.php');
     <link rel="stylesheet" href="css/table.css">
     <link rel="stylesheet" href="css/funcionario.css">
     <link rel="stylesheet" href="css/form_presen.css">
+    <link rel="stylesheet" href="css/popup.css">
     <link rel="apple-touch-icon" sizes="180x180" href="images/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="images/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="images/favicon-16x16.png">
