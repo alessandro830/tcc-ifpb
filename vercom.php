b5bccf719cef0c52ce2c7859bbeebe0ee3a452c9
Alessandro Viana <116918756+alessandro830@users.noreply.github.com>
2023-12-03 17:45:56 -0300

    página do comentário dos alunos

--- vercom.php
+++ vercom.php
@@ -1,49 +1,32 @@
+<?php
+
+include('php/protect.php');
+
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
-<meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/navbar.css">
-    <link rel="stylesheet" href="css/table.css">
-    <link rel="stylesheet" href="css/funcionario.css">
-    <link rel="apple-touch-icon" sizes="180x180" href="images/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="images/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="images/favicon-16x16.png">
-    <title>Comentário</title>
+    <link rel="stylesheet" href="css/comentarios.css">
+    <title>Comentário dos alunos</title>
 </head>
-<header>
+<body>
+    <header>
         <img src="images/ifpb-horizontal200.png" alt="logo horizontal do ifpb">
         <div class="nav_buttons">
             <a href="funcionario.php"><button>Voltar</button></a>
         </div>
     </header>
     <main>
-    <div class="box">
-            <table>
-                <caption>Comentarios sobre a refeição</caption>
-                <thead>
-                    <tr>
-                        <th>Nome</th>
-                        <th>Matrícula</th>
-                        <th>Comentário</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php
-                    $con = mysqli_connect("localhost", "root", "", "marmita");
-                    $select = "SELECT * FROM alunos WHERE feedback IS NOT NULL";
-                    $result = mysqli_query($con, $select) or die (mysqli_error($con));
-                    while($linha = mysqli_fetch_array($result)){
-                        echo "<tr>";
-                            echo "<td>" . $linha['nome'] . "</td>";
-                            echo "<td>" . $linha['matricula'] . "</td>";
-                            echo "<td>" . $linha['feedback'] . "</td>";
-                        echo "<tr>";
-                    }
-                    ?>
-                </tbody>
-            </table>
+        <div class="box">
+            <h1>Comentário dos alunos</h1>
+            <div class="coment">
+                <p><strong>(Nome do aluno:)</strong> (Comentário do aluno, não se esquerecer do espaço no início)</p>
+            </div>
+            <hr> <!-- Repetir essa tag no final dos comentários -->
         </div>
     </main>
 </body>
