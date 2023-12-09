ef57e5baa8684044c92ca632a2b0156eae4c50c1
Samuel <124095857+SamuelMorais45@users.noreply.github.com>
2023-12-09 11:08:01 -0300

    O dia agora é selecionado automaticamente de acordo com o dia atual

--- php/regis_extra.php
+++ php/regis_extra.php
@@ -1,10 +1,13 @@
 <?php
+include('horario.php');
+
 $mat = $_POST['matricula'];
 $nome = $_POST['nome'];
-$dia = $_POST['date'];
+$dia = date('D');
+$dia_portugues = obter_dia($dia);
 $connect = mysqli_connect("localhost", "root", "", "marmita");
 $insert = mysqli_prepare($connect, "INSERT INTO extras (mat, nome, dia) VALUES (?, ?, ?)");
-mysqli_stmt_bind_param($insert, "iss", $mat, $nome, $dia);
+mysqli_stmt_bind_param($insert, "iss", $mat, $nome, $dia_portugues);
 mysqli_stmt_execute($insert);
 
 echo header("location:../extras.php");
